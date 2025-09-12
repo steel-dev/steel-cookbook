a81258068584dcdf36250dc87f5412030bb922b7
aspectrr <collin@steelbrowser.com>
2025-09-12 09:07:37 -0400

    fix: retry logic and more specific locators

--- examples/steel-extensions-starter/index.ts
+++ examples/steel-extensions-starter/index.ts
@@ -5,9 +5,8 @@
 
 import dotenv from "dotenv";
 import { chromium } from "playwright";
-import { buildTable, showAvatar, Stats } from "./table";
-import chalk from "chalk";
 import Steel from "steel-sdk";
+import { scrapeStats } from "./stats";
 
 dotenv.config();
 
@@ -120,114 +119,19 @@ async function main() {
       ];
     }; // Switch out for your GitHub username!
 
-    const username = await randomContributor();
+    let retries = 0;
+    let username;
 
-    console.log(`Navigating to ${username}'s GitHub Profile`);
-
-    await page.goto(`https://github.com/${username}`, {
-      waitUntil: "domcontentloaded",
-      timeout: 30000,
-    });
-
-    // Scroll to contributions section
-    const contributionsSection = page
-      .locator("#js-contribution-activity-description")
-      .first();
-
-    await contributionsSection.scrollIntoViewIfNeeded();
-
-    // Wait for the graph to load
-    await page.waitForSelector('div:has(h5:text("Contributions"))');
-
-    const wrapper = page.locator('div:has(h5:text("Contributions"))');
-
-    const contributions = await wrapper
-      .locator("div.p-2")
-      .nth(0)
-      .locator("span.f2")
-      .innerText();
-
-    const totalRange = await wrapper
-      .locator("div.p-2")
-      .nth(0)
-      .locator("span.color-fg-muted")
-      .innerText();
-
-    const thisWeek = await wrapper
-      .locator("div.p-2")
-      .nth(1)
-      .locator("span.f2")
-      .innerText();
-
-    const thisWeekRange = await wrapper
-      .locator("div.p-2")
-      .nth(1)
-      .locator("span.color-fg-muted")
-      .innerText();
-
-    const bestDay = await wrapper
-      .locator("div.p-2")
-      .nth(2)
-      .locator("span.f2")
-      .innerText();
-
-    const bestDayDate = await wrapper
-      .locator("div.p-2")
-      .nth(2)
-      .locator("span.color-fg-muted")
-      .innerText();
-
-    const averagePerDay = await wrapper.locator("p span.text-bold").innerText();
-
-    await page.waitForSelector('div:has(h5:text("Streaks"))');
-
-    const streakWrapper = page.locator('div:has(h5:text("Streaks"))');
-
-    const streakLongestText = await streakWrapper
-      .locator("div.p-2")
-      .nth(0)
-      .locator("span.f2")
-      .innerText();
-    const streakLongest = parseInt(streakLongestText, 10);
-    const streakLongestRange = await streakWrapper
-      .locator("div.p-2")
-      .nth(0)
-      .locator("span.color-fg-muted")
-      .innerText();
-
-    const streakCurrentText = await streakWrapper
-      .locator("div.p-2")
-      .nth(1)
-      .locator("span.f2")
-      .innerText();
-    const streakCurrent = parseInt(streakCurrentText, 10);
-    const streakCurrentRange = await streakWrapper
-      .locator("div.p-2")
-      .nth(1)
-      .locator("span.color-fg-muted")
-      .innerText();
-
-    const stats: Stats = {
-      username,
-      contributions,
-      totalRange,
-      thisWeek,
-      thisWeekRange,
-      bestDay,
-      bestDayDate,
-      averagePerDay,
-      streakLongest,
-      streakLongestRange,
-      streakCurrent,
-      streakCurrentRange,
-    };
-
-    console.log(chalk.bold.cyan(`\n📊 GitHub Stats for ${stats.username}\n`));
-
-    const avatar = await showAvatar(stats.username);
-    console.log(avatar);
-
-    console.log(buildTable(stats));
+    while (retries < 3) {
+      try {
+        username = await randomContributor();
+        await scrapeStats(page, username);
+        break;
+      } catch (error) {
+        console.error(`Failed to navigate to ${username}'s profile: ${error}`);
+        retries++;
+      }
+    }
 
     // ============================================================
     // End of Automations
