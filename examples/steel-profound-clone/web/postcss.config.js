12ac70c5ab776dd0ddf4e530e410ccd012ab134b
aspectrr <collin@steelbrowser.com>
2025-11-11 05:15:31 -0500

    feat: start wokring on profound clone

---
+++ examples/steel-profound-clone/web/postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * PostCSS configuration for Vite + React + Tailwind CSS
+ * Tailwind directives are processed here and autoprefixer adds vendor prefixes.
+ */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
